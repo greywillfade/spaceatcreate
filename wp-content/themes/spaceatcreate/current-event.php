74662f11b04765328c93f3b92c16fbb110741fec
SJ <sally.jenkinson@gmail.com>
2013-02-08 13:08:18 +0000

    Update wp-content/themes/spaceatcreate/current-event.php
    Edited template name, h1, and query (to return a SINGLE current event)

--- wp-content/themes/spaceatcreate/current-event.php
+++ wp-content/themes/spaceatcreate/current-event.php
@@ -1,23 +1,29 @@
-<?php /* Template Name: Past Events */
+<?php /* Template Name: Current Event */
 get_header(); ?>
 	
 	<div role="document">
 		<div class="row main" role="main">
-			<h1>Past Exhibitions</h1>
-			<?php global $querystr, $today; // not sure this is required in 3+
+			<h1>Current Exhibition</h1>
+			<?php global $querystr; // not sure this is required in 3+
 			
 			date_default_timezone_set('GMT');
-			$today = date('d/m/y');
 			
 			$querystr = "
-				SELECT $wpdb->posts.*
-				FROM $wpdb->posts, $wpdb->postmeta
-				WHERE $wpdb->posts.ID = $wpdb->postmeta.post_id 
-				AND $wpdb->posts.post_type = 'event' 
-				AND $wpdb->posts.post_status = 'publish' 
-				AND $wpdb->postmeta.meta_key = 'event_end_date'
-				AND $wpdb->postmeta.meta_value < '" . $today . "'
-				ORDER BY $wpdb->posts.post_date DESC 
+				SELECT p.*, pm.meta_key AS mk1, pm.meta_value AS eventstartdate, pm2.meta_key AS mk2, pm2.meta_value AS eventenddate
+				FROM $wpdb->posts AS p
+				LEFT JOIN $wpdb->postmeta AS pm
+					ON (p.ID = pm.post_id)
+				LEFT JOIN $wpdb->postmeta AS pm2
+					ON (p.ID = pm2.post_id)
+				WHERE
+					p.post_type = 'event' 
+				AND p.post_status = 'publish' 
+				AND pm.meta_key = 'event_start_date'
+				AND pm2.meta_key = 'event_end_date'
+				AND DATEDIFF(STR_TO_DATE(pm.meta_value,'%d/%m/%Y'),date(now())) <= 0
+				AND DATEDIFF(STR_TO_DATE(pm2.meta_value,'%d/%m/%Y'),date(now())) >= 0
+				ORDER BY p.post_date ASC
+				LIMIT 1
 			";
 			
 			$event_posts = $wpdb->get_results($querystr, OBJECT);
